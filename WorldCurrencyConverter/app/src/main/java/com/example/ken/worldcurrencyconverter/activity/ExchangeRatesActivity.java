be5de845de69065d6919c4c9a13e7c2057f74ef0
Ken Mau <ken@getyestap.com>
2017-04-24 17:46:15 -0400

    KenM - valid number checking, internet connection checks, polish, add button

--- WorldCurrencyConverter/app/src/main/java/com/example/ken/worldcurrencyconverter/activity/ExchangeRatesActivity.java
+++ WorldCurrencyConverter/app/src/main/java/com/example/ken/worldcurrencyconverter/activity/ExchangeRatesActivity.java
@@ -1,6 +1,9 @@
 package com.example.ken.worldcurrencyconverter.activity;
 
 import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -13,11 +16,14 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.ken.worldcurrencyconverter.R;
 import com.example.ken.worldcurrencyconverter.adapter.CurrencyAdapter;
@@ -28,6 +34,8 @@ import com.example.ken.worldcurrencyconverter.webclient.ApiInterface;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.net.UnknownHostException;
+import java.text.NumberFormat;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -52,11 +60,14 @@ public class ExchangeRatesActivity extends AppCompatActivity {
     private static final String TAG = ExchangeRatesActivity.class.getSimpleName();
     private static final int DEBOUNCE_THRESHOLD = 500; // in ms
     private static final int GRID_LAYOUT_SPAN_COUNT = 2; // show 2 cards width wise
+    private static final int DOLLARS_MAX_LENGTH = 10;
 
     // Layout Views
+    private CoordinatorLayout mParentLayout;
     private EditText mDollarsEditText;
     private EditText mCentsEditText;
     private Spinner mCurrencyCodeSpinner;
+    private Button mGoButton;
     private RecyclerView mRecyclerViewCurrencies;
 
     private String mBaseCurrencySelected;
@@ -72,6 +83,32 @@ public class ExchangeRatesActivity extends AppCompatActivity {
     private Disposable _disposable;
 
     // Setup RxAndroid Observables
+    private Observable<String> createGoButtonClickObservable() {
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(final ObservableEmitter<String> e) throws Exception {
+                if (mGoButton != null) {
+                    mGoButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            hideKeyboard();
+
+                            e.onNext("");
+                        }
+                    });
+
+                    e.setCancellable(new Cancellable() {
+                        @Override
+                        public void cancel() throws Exception {
+                            mGoButton.setOnClickListener(null);
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
     private Observable<String> createDollarsTextChangeObservable() {
         final Observable<String> textChangeObservable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
@@ -164,13 +201,7 @@ public class ExchangeRatesActivity extends AppCompatActivity {
                 String item = (String) parent.getItemAtPosition(position);
                 mBaseCurrencySelected = item;
 
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                if (mDollarsEditText != null && mDollarsEditText.hasWindowFocus()) {
-                    imm.hideSoftInputFromWindow(mDollarsEditText.getWindowToken(), 0);
-                }
-                if (mCentsEditText != null && mCentsEditText.hasWindowFocus()) {
-                    imm.hideSoftInputFromWindow(mCentsEditText.getWindowToken(), 0);
-                }
+                hideKeyboard();
 
                 selectSubject.onNext(item);
             }
@@ -188,11 +219,15 @@ public class ExchangeRatesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange_rates);
 
+        // Initialize EditText Soft Keyboard Properties
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         // Initialize layout properties
+        mParentLayout = (CoordinatorLayout) findViewById(R.id.parentLayout);
         mDollarsEditText = (EditText) findViewById(R.id.etDollars);
         mCentsEditText = (EditText) findViewById(R.id.etCents);
         mCurrencyCodeSpinner = (Spinner) findViewById(R.id.spinCurrencyCode);
+        mGoButton = (Button) findViewById(R.id.bGo);
         mRecyclerViewCurrencies = (RecyclerView) findViewById(R.id.rvCurrencies);
 
         // Setup Recycler View Dependencies
@@ -209,14 +244,12 @@ public class ExchangeRatesActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.currency_code_array, android.R.layout.simple_spinner_dropdown_item);
         mCurrencyCodeSpinner.setAdapter(spinnerAdapter);
 
-
-
         // Setup Web Client
         mApiService = ApiClient.getClient().create(ApiInterface.class);
 
         // Intercept any non-digits and shift in focus to cents field
         if (mDollarsEditText != null) {
-            mDollarsEditText.setFilters(new InputFilter[]{
+            mDollarsEditText.setFilters(new InputFilter[] {
                     new InputFilter() {
                         @Override
                         public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
@@ -232,7 +265,8 @@ public class ExchangeRatesActivity extends AppCompatActivity {
                             // Filter out any non-digits
                             return "";
                         }
-                    }
+                    },
+                    new InputFilter.LengthFilter(DOLLARS_MAX_LENGTH)
             });
         }
     }
@@ -244,7 +278,10 @@ public class ExchangeRatesActivity extends AppCompatActivity {
         Observable<String> amountTextChangeStream = Observable.merge(createDollarsTextChangeObservable(), createCentsTextChangeObservable());
         Observable<String> currencyCodeChangeStream = createCurrencyCodeSpinnerObservable();
 
-        Observable<String> inputChangeStream = Observable.merge(amountTextChangeStream, currencyCodeChangeStream);
+        Observable<String> inputChangeStream = Observable
+                .merge(createDollarsTextChangeObservable(), createCentsTextChangeObservable())
+                .mergeWith(createCurrencyCodeSpinnerObservable())
+                .mergeWith(createGoButtonClickObservable());
 
         _disposable = inputChangeStream
                 // On the UI Thread
@@ -296,7 +333,15 @@ public class ExchangeRatesActivity extends AppCompatActivity {
 
                                     @Override
                                     public void onError(Throwable e) {
-                                        // TODO Handle errors
+                                        // Handle network errors
+                                        if (e instanceof UnknownHostException) {
+                                            Snackbar snackbar = Snackbar
+                                                    .make(mParentLayout, "Check Internet Connection!", Snackbar.LENGTH_LONG);
+
+                                            View sbView = snackbar.getView();
+                                            TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+                                            snackbar.show();
+                                        }
                                         e.printStackTrace();
                                     }
 
@@ -306,14 +351,24 @@ public class ExchangeRatesActivity extends AppCompatActivity {
                                         int dollars = 0, cents = 0;
                                         double combinedAmount;
 
-                                        if (!TextUtils.isEmpty(mDollarsEditText.getText().toString())) {
-                                            dollars = Integer.parseInt(mDollarsEditText.getText().toString());
+                                        if (mDollarsEditText != null && !TextUtils.isEmpty(mDollarsEditText.getText().toString())) {
+                                            try {
+                                                dollars = Integer.parseInt(mDollarsEditText.getText().toString());
+                                            } catch (NumberFormatException e) {
+                                                mDollarsEditText.setError("Please enter an amount between 0 and " + Integer.MAX_VALUE);
+                                            }
+
                                         }
 
-                                        if (!TextUtils.isEmpty(mCentsEditText.getText().toString())) {
-                                            cents = Integer.parseInt(mCentsEditText.getText().toString());
+                                        if (mCentsEditText != null && !TextUtils.isEmpty(mCentsEditText.getText().toString())) {
+                                            try {
+                                                cents = Integer.parseInt(mCentsEditText.getText().toString());
+                                            } catch (NumberFormatException e) {
+                                                mDollarsEditText.setError("Please enter an amount between 0 and " + Integer.MAX_VALUE);
+                                            }
                                         }
 
+                                        // Assumption: Cents will only have 2 decimal places.  We could have more, we just have to do extra handling.
                                         combinedAmount = dollars + cents/100.0;
 
                                         mRecyclerViewAdapter.clearRates();
@@ -344,6 +399,19 @@ public class ExchangeRatesActivity extends AppCompatActivity {
 
         if (_disposable != null && !_disposable.isDisposed()) {
             _disposable.dispose();
+        }
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        getCurrentFocus().clearFocus();
+
+        if (mDollarsEditText != null) {
+            mDollarsEditText.clearFocus();
+        }
+        if (mCentsEditText != null) {
+            mCentsEditText.clearFocus();
         }
     }
 }
